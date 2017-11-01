5823e0053bcddc095cad7c80acb583ae143cc12f
amir <amir@ibggn34885-l.ibtechnology.com>
2017-11-01 17:15:42 +0530

    Added baseline image for note 3, nexus 5, iphone 6S Plus and iPhone 5

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -40,7 +40,7 @@ var capsConfig = {
     'os_version' : '10',
     'chromeOptions': {
       'mobileEmulation': {
-        'deviceName': 'Nexus 5'
+        'deviceName': 'Nexus 5X'
       }
     }
   },
@@ -106,7 +106,7 @@ var capsConfig = {
     'os_version' : '10',
     'chromeOptions': {
       'mobileEmulation': {
-        'deviceName': 'iPad'
+        'deviceName': 'iPad Pro'
       }
     }
   },
@@ -118,6 +118,17 @@ var capsConfig = {
     'chromeOptions': {
       'mobileEmulation': {
         'deviceName': 'iPhone 6 Plus'
+      }
+    }
+  },
+  'iphone5': {
+    'browser' : 'Chrome',
+    'browser_version' : '61.0',
+    'os' : 'Windows',
+    'os_version' : '10',
+    'chromeOptions': {
+      'mobileEmulation': {
+        'deviceName': 'iPhone 5'
       }
     }
   }
@@ -145,6 +156,7 @@ describe('Visual monitor testing', function() {
   after(function(done) {
     shoovWebdrivercss.after(done);
   });
+
 
   // Method to take HomePage screenshots
   it('should open OneChampionShip homepage',function(done) {
@@ -175,15 +187,16 @@ describe('Visual monitor testing', function() {
   });
 
   //Methods to take sceenshot of T-SHIRT category page after login
-  it('should redirect on t-shirt page after login',function(done) {
+  it('should open t-shirt page after login',function(done) {
     client
       .url(baseUrl + 'customer/account/login/')
-      .setValue('.//*[@id="email"]','nitesh.sharma@incedoinc.com')
+      .setValue('.//*[@id="email"]','shoov.test@ui.com')
       .setValue('.//*[@id="pass"]','asl@12345')
       .submitForm('.//*[@id="send2"]')
       .pause(5000)
       .webdrivercss(testName + '.tshirt_category_page_after_login', {
         name: '1',
+        //exclude: ['.//*[@id="category-products"]/div/div/div/ol/li'],
         exclude: [],
         remove: [],
         hide: [],
@@ -193,7 +206,7 @@ describe('Visual monitor testing', function() {
   });
 
   //Methods to take sceenshot of MENS category page after login
-  it('should redirect on mens category page after login',function(done) {
+  it('should open mens category page after login',function(done) {
     client
       .url(baseUrl)
       .click('.navbar-toggler.navbar-toggler-right')
@@ -211,7 +224,7 @@ describe('Visual monitor testing', function() {
   });
 
   //Methods to take sceenshot of WOMEN category page after login
-  it('should redirect on women category page after login',function(done) {
+  it('should open women category page after login',function(done) {
     client
       .url(baseUrl)
       .click('.navbar-toggler.navbar-toggler-right')
@@ -227,5 +240,245 @@ describe('Visual monitor testing', function() {
       }, resultsCallback)
       .call(done);
   });
+
+  //Methods to take sceenshot of OTHERS category page after login
+  it('should open others category page after login',function(done) {
+    client
+      .url(baseUrl)
+      .click('.navbar-toggler.navbar-toggler-right')
+      .pause(5000)
+      .click('.//*[@id="navbarSupportedContent"]/ul/li[4]/a')
+      .pause(5000)
+      .webdrivercss(testName + '.others_category_page_after_login', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of product detail page after adding product into cart
+  it('should open product details page and adding product into cart',function(done) {
+    client
+      .url(baseUrl + 'logo-unisex-sleeves-ragan')      
+      .pause(50000)
+      .scroll('.//*[@id="product_addtocart_form"]/div[2]/div/div/div/div[1]')
+      .click('.//*[@id="product_addtocart_form"]/div[2]/div/div/div/div[1]')
+      .click('.//*[@id="product-addtocart-button"]')
+      .pause(50000)
+      .webdrivercss(testName + '.product_detail_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of shopping cart page
+  it('should open shopping cart page',function(done) {
+    client
+      .url(baseUrl + 'checkout/cart/')
+      .pause(10000)
+      .webdrivercss(testName + '.shopping_cart_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of checkout page
+  it('should open checkout page',function(done) {
+    client
+      .url(baseUrl + 'checkout/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.checkout_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of My Account page
+  it('should open my account page',function(done) {
+    client
+      .url(baseUrl + 'customer/account/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.my_account_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of Account Info page
+  it('should open account info page',function(done) {
+    client
+      .url(baseUrl + 'customer/account/edit/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.account_info_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of Address Book page
+  it('should open address book page',function(done) {
+    client
+      .url(baseUrl + 'customer/address/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.address_book_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of My Orders page
+  it('should open my orders page',function(done) {
+    client
+      .url(baseUrl + 'sales/order/history/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.my_orders_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+  
+
+  //Methods to take sceenshot of Newsletter Subscription page
+  it('should open newsletter subscription page',function(done) {
+    client
+      .url(baseUrl + 'newsletter/manage/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.newsletter_subscription_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of wishlist page
+  it('should open wishlist page',function(done) {
+    client
+      .url(baseUrl + 'wishlist/')   
+      .pause(50000)   
+      .webdrivercss(testName + '.wishlist_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of payment method page
+  it('should open payment method page',function(done) {
+    client
+      .url(baseUrl + 'checkout/#payment')   
+      .pause(50000)   
+      .scroll('.//*[@id="adyen_hpp"]')
+      .click('.//*[@id="adyen_hpp"]') 
+      .pause(50000)      
+      .webdrivercss(testName + '.payment_method_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of adyen payment method page
+  it('should open adyen payment method page',function(done) {
+    client
+      .url(baseUrl + 'checkout/#payment')   
+      .pause(50000)        
+      .scroll('.//*[@id="checkout-payment-method-load"]/div/div/div[2]/div[2]/div[3]/div/button')
+      .click('.//*[@id="checkout-payment-method-load"]/div/div/div[2]/div[2]/div[3]/div/button')
+      .pause(50000) 
+      .webdrivercss(testName + '.adyen_payment_method_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+
+  //Methods to take sceenshot of select master card method page
+  it('should open select master card payment method page',function(done) {
+    client
+      //.url(baseUrl + 'checkout/#payment')      
+      .scroll('.//*[@id="paymentMethods"]/li[7]/input')
+      .click('.//*[@id="paymentMethods"]/li[7]/input')
+      .pause(50000) 
+      .webdrivercss(testName + '.mastercard_payment_method_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  //Methods to take sceenshot of thank you for your order page
+  it('should open thank you for your order page',function(done) {    
+    client
+      .scroll('.//*[@id="card.cardNumber"]')
+      .setValue('.//*[@id="card.cardNumber"]','2223000048410010')
+      .setValue('.//*[@id="card.cardHolderName"]','SHOOV TEST')
+      .selectByVisibleText('.//*[@id="card.expiryMonth"]','08')
+      .selectByVisibleText('.//*[@id="card.expiryYear"]','2018')
+      .setValue('.//*[@id="card.cvcCode"]','737')
+      .scroll('.//*[@id="mainSubmit"]')
+      .submitForm('.//*[@id="mainSubmit"]')
+      .pause(10000) 
+      .scroll('.//*[@id="mainSubmit"]')
+      .submitForm('.//*[@id="mainSubmit"]')
+      .pause(50000) 
+      .webdrivercss(testName + '.thankyou_for_your_order_page', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+
+
 });
 
